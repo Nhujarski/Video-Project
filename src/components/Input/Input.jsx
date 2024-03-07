45a69566fb6214641a36f91a5c9b1331fee1bd4d
Nick Hujarski <nickhujarski@Roberts-13-inch-MacBook-Pro.local>
2024-03-06 20:55:47 -0800

    added input component and styles. started card. going to move to its own branch

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,18 +1,15 @@
 import React, { useState } from 'react';
 import './input.css';
 
-const Input = ({ placeHolder, labelId, labelText }) => {
+const Input = ({ placeHolder, type }) => {
   const [inputVal, setInputVal] = useState('');
 
   return (
     <>
-      <label for=''>{labelText}</label>
       <input
         className='form-input'
-        id={labelId}
-        type='text'
+        type={type}
         onChange={(e) => {
-          console.log(e.target.value);
           setInputVal(e.target.value);
         }}
         value={inputVal}
